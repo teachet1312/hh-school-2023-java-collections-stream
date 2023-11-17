577d3f5ca52ac5ee2cb20e13b4fa69c142d7e865
teachet1312 <iceninen@gmail.com>
2023-11-17 14:14:18 +0300

    2 task done

--- src/main/java/tasks/Task2.java
+++ src/main/java/tasks/Task2.java
@@ -1,9 +1,9 @@
 package tasks;
 
 import common.Person;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 /*
 Задача 2
@@ -16,6 +16,14 @@ public class Task2 {
   public static List<Person> combineAndSortWithLimit(Collection<Person> persons1,
                                                      Collection<Person> persons2,
                                                      int limit) {
-    return new ArrayList<>();
+    persons1.addAll(persons2);
+    List<Person> persons_ = persons1.stream()
+            .sorted(Comparator.comparing(Person::getCreatedAt))
+            .collect(Collectors.toList());
+    List<Person> result = new ArrayList<>();
+    for (int n = 0; n < limit; n++) {
+      result.add(persons_.get(n));
+    }
+    return result;
   }
 }
